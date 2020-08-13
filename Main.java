d30668fe60861d7a92bedc7b7a23077c0c32eb2a
TS80204 <69643335+TS80204@users.noreply.github.com>
2020-08-13 10:55:45 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String args[]) {
+	System.out.println("One of my hobbies is going to the shooting range to improve my accuracy.");
+	System.out.println("My favorite subjects are physics, calculus, and computer science.");
+	System.out.println("In my free time I like to go on walks and hike.");
+	System.out.println("My dream university is UC Berkeley.");
+	System.out.println("I want to major in Computer Science.");
+	System.out.println("A cool fact about me is that I have had three concussions up till now.");
+	}
+	
+}
